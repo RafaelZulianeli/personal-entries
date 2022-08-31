8067a38895a7bcd5a55299a5ecbb483d7ebed8e2
Rafael Zulianeli <rzrsantos@gmail.com>
2022-08-31 16:17:02 -0300

    feat: update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   cms: {
     domain: 'https://cloud.squidex.io/',
+    apiContent: 'api/content/personal-entries-squidex/graphql/',
     client_id: 'personal-entries-squidex:front-end',
     client_secret: 'vvaioiyulgnxlv7kbzyx5t5xa5eehinipxeuqqe01aex'
   }
