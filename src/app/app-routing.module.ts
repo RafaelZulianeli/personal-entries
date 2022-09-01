79b27d568d5ee8f72d0dbd01ae9c9d5422c81609
Rafael Zulianeli <rzrsantos@gmail.com>
2022-08-31 23:41:22 -0300

    feat: adjust entries routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'entries',
+    loadChildren: () => import('./entries/entries.module').then((m) => m.EntriesModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'entries',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
